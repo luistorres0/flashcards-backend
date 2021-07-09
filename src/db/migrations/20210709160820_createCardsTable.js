b3a8b3d83bb0086495d4bb556c6ba0ef60de4506
Luis Torres <luisdtor@gmail.com>
2021-07-09 11:20:01 -0500

    Created migration for 'cards' table

---
+++ src/db/migrations/20210709160820_createCardsTable.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("cards", (table) => {
+    table.increments("id").primary();
+    table.text("front").notNullable();
+    table.text("back").notNullable();
+    table.integer("deckId").unsigned().notNullable();
+    table.foreign("deckId").references("id").inTable("decks");
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("cards");
+};
