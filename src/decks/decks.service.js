af9344ed8fb7c320ed1e52d0a880f1e9c754ccf5
Luis Torres <luisdtor@gmail.com>
2021-08-06 17:28:02 -0500

    Updated the get-decks endpoint so that each deck object now has a 'cards' key pointing to all cards in the deck

--- src/decks/decks.service.js
+++ src/decks/decks.service.js
@@ -1,9 +1,21 @@
 const knex = require("../db/connection");
+const cardsService = require("../cards/cards.service");
 
 function list() {
-  return knex("decks").select("*");
+  return knex("decks")
+    .select("*")
+    .then((decks) => {
+      const formattedDecks = decks.map((deck) => {
+        return cardsService.listCardsInDeck(deck.id).then((cards) => {
+          deck.cards = cards;
+          return deck;
+        });
+      });
+
+      return Promise.all(formattedDecks);
+    });
 }
 
 module.exports = {
-    list
-}
+  list,
+};
